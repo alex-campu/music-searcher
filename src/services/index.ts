6c6b94528a6b41b10f145f2863e95c1370aa5985
Alex Campu <alex.campuzano316@gmail.com>
2022-07-25 14:15:19 -0500

    set up services

---
+++ src/services/index.ts
@@ -0,0 +1,22 @@
+import axios from 'axios'
+import { API_URLS } from './servicesRoutes'
+
+const token = process.env.VITE_TOKEN
+
+type paramsType = {
+  [key: string]: string | undefined
+}
+const paramsHandler = (route: string, params?: paramsType) => {
+  if (params?.token) {
+    return params?.query ? `${route}?token=${token}&query=${params.query}` : `${route}?token=${token}`
+  }
+  return params?.query ? `${route}?query=${params.query}` : route
+}
+
+export const generalService = (route: string) => {
+  return ({
+    get: async (query?: string) => axios.get(paramsHandler(route, { token, query })),
+  })
+}
+
+export const releasesService = generalService(API_URLS.RELEASES)
